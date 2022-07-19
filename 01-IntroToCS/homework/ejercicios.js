417167eb5293f1cab85d7e56db510753d46dd806
Agustin Fraile <agustinfraile98@gmail.com>
2022-07-19 09:22:08 -0300

    iniciando repo

---
+++ 01-IntroToCS/homework/ejercicios.js
@@ -0,0 +1,17 @@
+function DecimalABinario(num) {
+    
+    let restoDivision;
+
+    // bucle para ir dividiendo el numero en 2 hasta que el resultado sea menor que 1
+    while ( num / 2 > 1  ) {
+        num
+        restoDivision =  num % 2;
+        restoDivision;
+
+        num = Math.trunc(num / 2);
+        num
+    }
+    
+}
+
+DecimalABinario('9')
