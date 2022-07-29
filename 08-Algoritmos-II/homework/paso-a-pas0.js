d539fc15322fa9b11d45cc6eeb67926bdf62c610
Agustin Fraile <agustinfraile98@gmail.com>
2022-07-29 08:06:58 -0300

    resolviendo hw 08

---
+++ 08-Algoritmos-II/homework/paso-a-pas0.js
@@ -0,0 +1,23 @@
+function quickSort(array){
+//si el arreglo ya está ordenado, retornarlo
+//crear una variable pivot que sea igual a array.length - 1
+//crear un nuevo arreglo para almacenar todos los valores menores al pivote = []
+//crear un nuevo arreglo para almacenar todos los valores mayores o iguales al pivote = [4, 1, 7, 2, 9, 10, 3, 2]
+
+// arreglo ordenado = valores menores + pivote + valores mayores o iguales
+// retornar el arreglo ordenado
+}
+
+function mergeSort(array){
+// si el arreglo tiene longitud 1, retornarlo
+// dividir el arreglo en partes iguales
+// 
+}
+
+function unificar(left, right){
+  // asumir que left y right son dos arreglos ordenados
+  // recorrer los arreglos left y right desde el primer elemento hasta el último
+  // comparar un elemento de left con uno de right
+  // pushear a un nuevo arreglo el valor menor
+  // incrementar la variable que está recorriendo el arreglo del cual obtuvimos el valor menor
+}
